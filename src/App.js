f1645a0736c0f93e3ca6b8b3608d82bcb16c4727
Bowen Zhang <bowenzhang@gmail.com>
2021-01-28 19:46:13 -0500

    initial setup!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styles from "./App.module.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      hingeData: "",
+    };
+  }
+
+  //   handleHingeDataSubmission(event) {
+  //     this.setState({
+  //       hingeData: event.target.data,
+  //     });
+  //     console.log(this.state);
+  //   }
+
+  render() {
+    return (
+      <>
+        <div className={styles.Logo}>DateDash.Me</div>
+        <div className={styles.HowToUse}>
+          Some filler text on how to use this!
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
