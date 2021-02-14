a226b5d5c00a833535ae108f4d8382e9b8640f8e
Bowen Zhang <bowenzhang@gmail.com>
2021-02-14 12:05:58 -0500

    finished sankey chart

--- src/api/processHingeData.js
+++ src/api/processHingeData.js
@@ -75,6 +75,97 @@ const getTheyLikedMe = (rawHingeData) => {
   return count;
 };
 
+const getTheyLikedFizzle = (rawHingeData) => {
+  let count = 0;
+  for (let interactionNum in rawHingeData) {
+    if (
+      "match" in rawHingeData[interactionNum] &&
+      !("like" in rawHingeData[interactionNum]) &&
+      "chats" in rawHingeData[interactionNum] &&
+      rawHingeData[interactionNum]["chats"].length < 5
+    ) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+const getTheyLikedConversation = (rawHingeData) => {
+  let count = 0;
+  for (let interactionNum in rawHingeData) {
+    if (
+      "match" in rawHingeData[interactionNum] &&
+      !("like" in rawHingeData[interactionNum]) &&
+      "chats" in rawHingeData[interactionNum] &&
+      rawHingeData[interactionNum]["chats"].length >= 5
+    ) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+const getTheyLikedGhost = (rawHingeData) => {
+  let count = 0;
+  for (let interactionNum in rawHingeData) {
+    if (
+      "match" in rawHingeData[interactionNum] &&
+      !("like" in rawHingeData[interactionNum]) &&
+      !("chats" in rawHingeData[interactionNum])
+    ) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+const getILikedFizzle = (rawHingeData) => {
+  let count = 0;
+  for (let interactionNum in rawHingeData) {
+    if (
+      "match" in rawHingeData[interactionNum] &&
+      "like" in rawHingeData[interactionNum] &&
+      "chats" in rawHingeData[interactionNum] &&
+      rawHingeData[interactionNum]["chats"].length < 5
+    ) {
+      count += 1;
+      console.log(rawHingeData[interactionNum]);
+    }
+  }
+  return count;
+};
+
+const getILikedConversation = (rawHingeData) => {
+  let count = 0;
+  for (let interactionNum in rawHingeData) {
+    if (
+      "match" in rawHingeData[interactionNum] &&
+      "like" in rawHingeData[interactionNum] &&
+      "chats" in rawHingeData[interactionNum] &&
+      rawHingeData[interactionNum]["chats"].length >= 5
+    ) {
+      count += 1;
+      console.log(rawHingeData[interactionNum]);
+    }
+  }
+  return count;
+};
+
+const getILikedTheyGhost = (rawHingeData) => {
+  let count = 0;
+  for (let interactionNum in rawHingeData) {
+    if (
+      "match" in rawHingeData[interactionNum] &&
+      "like" in rawHingeData[interactionNum] &&
+      !("chats" in rawHingeData[interactionNum])
+    ) {
+      count += 1;
+      console.log(rawHingeData[interactionNum]);
+    }
+  }
+  return count;
+};
+
 const processHingeData = (rawHingeData) => {
   let processedData = {};
   processedData["Total Interactions"] = getTotalInteractions(rawHingeData);
@@ -84,15 +175,29 @@ const processHingeData = (rawHingeData) => {
   processedData["Swipe Right"] = getSwipeRight(rawHingeData);
   processedData["I Liked Them"] = getILikedThem(rawHingeData);
   processedData["They Liked Me"] = getTheyLikedMe(rawHingeData);
+  processedData["I Liked Fizzle"] = getILikedFizzle(rawHingeData);
+  processedData["I Liked Ghost"] = getILikedTheyGhost(rawHingeData);
+  processedData["I Liked Conversation"] = getILikedConversation(rawHingeData);
+  processedData["They Liked Fizzle"] = getTheyLikedFizzle(rawHingeData);
+  processedData["They Liked Ghost"] = getTheyLikedGhost(rawHingeData);
+  processedData["They Liked Conversation"] = getTheyLikedConversation(
+    rawHingeData
+  );
 
   let sankeyData = [
     ["From", "To", "Count"],
     ["Total Interactions", "Matched", processedData["Matched"]],
     ["Total Interactions", "No Match", processedData["No Match"]],
-    ["No Match", "Swipe Left", processedData["Swipe Left"]],
-    ["No Match", "Swipe Right", processedData["Swipe Right"]],
+    ["No Match", "Pass", processedData["Swipe Left"]],
+    ["No Match", "Like", processedData["Swipe Right"]],
     ["Matched", "I Liked Them", processedData["I Liked Them"]],
     ["Matched", "They Liked Me", processedData["They Liked Me"]],
+    ["I Liked Them", "Fizzle", processedData["I Liked Fizzle"]],
+    ["I Liked Them", "Ghost", processedData["I Liked Ghost"]],
+    ["I Liked Them", "Conversation", processedData["I Liked Conversation"]],
+    ["They Liked Me", "Fizzle", processedData["They Liked Fizzle"]],
+    ["They Liked Me", "Ghost", processedData["They Liked Ghost"]],
+    ["They Liked Me", "Conversation", processedData["They Liked Conversation"]],
   ];
   return sankeyData;
 };
--- src/components/HingeChart/HingeChart.jsx
+++ src/components/HingeChart/HingeChart.jsx
@@ -17,7 +17,7 @@ class HingeChart extends React.Component {
                 node: {
                   label: {
                     fontSize: 16,
-                    color: '#000',
+                    color: 'white',
                     },
                   interactivity: true, // Allows you to select nodes.
                   labelPadding: 6,     // Horizontal distance between the label and the node.
@@ -29,7 +29,21 @@ class HingeChart extends React.Component {
                     '#b2df8a',         // giving each node its own color.
                     '#33a02c'
                   ]
-                }
+                },
+                link: {
+                    color: {
+                      fill: '#efd',     // Color of the link.
+                      fillOpacity: 0.8, // Transparency of the link.
+                      stroke: 'black',  // Color of the link border.
+                      strokeWidth: 1    // Thickness of the link border (default 0).
+                    },
+                    colors: [
+                      '#a6cee3',        // Custom color palette for sankey links.
+                      '#1f78b4',        // Nodes will cycle through this palette
+                      '#b2df8a',        // giving the links for that node the color.
+                      '#33a02c'
+                    ]
+                  }
               }
         }}
         className={styles.HingeChart}
