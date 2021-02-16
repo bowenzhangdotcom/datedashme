898a1babb3c0ec3ee8cd7c59f62ada06c9161a77
Bowen Zhang <bowenzhang@gmail.com>
2021-02-15 21:19:08 -0500

    updated colors for donut

--- src/components/HingeDonutChart/HingeDonutChart.jsx
+++ src/components/HingeDonutChart/HingeDonutChart.jsx
@@ -13,30 +13,30 @@ class HingeChart extends React.Component {
         ariaTitle="Matches"
         constrainToVisibleArea={true}
         data={[
-          { x: 'I ghosted', y: hingeData["I Ghosted"] }, 
-          { x: 'They ghosted', y: hingeData["They Ghosted"] }, 
-          { x: 'Conversation fizzled out', y: hingeData["Fizzle"] }, 
-          { x: 'Conversation without a phone number', y: hingeData["Conversation"] },
-          { x: 'Conversation with a phone number', y: hingeData["Phone Conversation"] }, 
+          { x: 'I ghosted', y: hingeData["I Ghosted"], label: "test1" }, 
+          { x: 'They ghosted', y: hingeData["They Ghosted"], label: "test1" }, 
+          { x: 'Conversation fizzled out', y: hingeData["Fizzle"], label: "test1" }, 
+          { x: 'Conversation without a phone number', y: hingeData["Conversation"], label: "test1" },
+          { x: 'Conversation with a phone number', y: hingeData["Phone Conversation"], label: "test1" }, 
         ]}
-        legendData={[
-          { name: `I ghosted ${hingeData["I Ghosted"]}` }, 
-          { name: `They ghosted ${hingeData["They Ghosted"]}`}, 
-          { name: `Conversation fizzled out ${hingeData["Fizzle"]}` },
-          { name: `Conversation without a phone number ${hingeData["Conversation"]}` },
-          { name: `Conversation with a phone number ${hingeData["Phone Conversation"]}` }
-        ]}
-        legendOrientation="vertical"
-        legendPosition="right"
-        padding={{
-          bottom: 20,
-          left: 20,
-          right: 140,
-          top: 20
-        }}
-        // colorScale={["#D6E865"]}
+        // legendData={[
+        //   { name: `I ghosted ${hingeData["I Ghosted"]}` }, 
+        //   { name: `They ghosted ${hingeData["They Ghosted"]}`}, 
+        //   { name: `Conversation fizzled out ${hingeData["Fizzle"]}` },
+        //   { name: `Conversation without a phone number ${hingeData["Conversation"]}` },
+        //   { name: `Conversation with a phone number ${hingeData["Phone Conversation"]}` }
+        // ]}
+        // legendOrientation="vertical"
+        // legendPosition="right"
+        // padding={{
+        //   bottom: 20,
+        //   left: 20,
+        //   right: 140,
+        //   top: 20
+        // }}
+        //colorScale={["#E1E4D0", "#CBD0AD", "#BAC193", "#ADB57F", "#97A05C"]}
         innerRadius={69}
-        labels={({ datum }) => `${datum.x}: ${datum.y}%`}
+        // labels={({ datum }) => `${datum.x}: ${datum.y}%`}
         subTitle="Total Matches"
         title={hingeData["Matched"]}
         />
