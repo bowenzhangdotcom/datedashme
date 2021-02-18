5fdb8b7487a84d8064297d3da6ea4b4070d1bec4
Bowen Zhang <bowenzhang@gmail.com>
2021-02-17 23:55:43 -0500

    fixed submit button styling

--- src/components/FileSubmit/FileSubmit.jsx
+++ src/components/FileSubmit/FileSubmit.jsx
@@ -12,11 +12,14 @@ class FileSubmit extends React.Component {
 
   render() {
     return (
+      <div className={styles.FileSubmitRow}>
+      <span className={styles.ButtonLabel}>Add Your Own Data</span>
       <div className={styles.FileSubmit}>
         <label className={styles.CustomFileUpload}>
           <input className={styles.Input} type="file" onChange={this.props.handleFileChange} />        
-          Add Data
+          Upload
         </label>
+      </div>
       </div>
    )
   }
