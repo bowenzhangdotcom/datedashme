7f87e82b19e42a820be0635673cf18b64be3b6d0
Bowen Zhang <bowenzhang@gmail.com>
2021-02-24 18:38:57 -0500

    resized file upload

--- src/components/FileSubmit/FileSubmit.jsx
+++ src/components/FileSubmit/FileSubmit.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-//import styles from "./FileSubmit.module.css";
+import styles from "./FileSubmit.module.css";
 import { DropzoneArea } from 'material-ui-dropzone';
 import { Publish } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,11 +9,17 @@ const useStyles = makeStyles({
     background: '#e5e5e5',
     borderColor: '#734492',
   },
+  myDropZoneText: {
+    fontSize: "20px", 
+    paddingLeft: "2.5%", 
+    paddingRight: "2.5%",
+  }
 });
 
 export default function FileSubmit(props) {
   const classes = useStyles();
   return (
+    <div className={styles.DropzoneAreaContainer}>
       <DropzoneArea
         onChange={props.handleFileChange}
         dropzoneText={"Drag and drop your matches.json here or click"}
@@ -22,6 +28,8 @@ export default function FileSubmit(props) {
         Icon={Publish}
         useChipsForPreview ={true}
         dropzoneClass={classes.myDropZone}
+        dropzoneParagraphClass={classes.myDropZoneText}
       />
+    </div>
    )
   }
